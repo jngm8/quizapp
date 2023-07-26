12a7644060b9e822fa49284108efa94a246b4e5a
Jairo <jn.gomezm1@unaindes.edu.co>
2023-07-26 16:00:30 -0500

    All questions can be displayed

--- lib/model/quiz_questions.dart
+++ lib/model/quiz_questions.dart
@@ -5,4 +5,12 @@ class QuizQuestion{
 
   final String question;
   final List<String> answers;
+
+
+  List<String> getSuffledAnswers(){
+    
+    final copyList = List.of(answers); //List.of generates a copy of the original answer lis in order to not modify it.
+    copyList.shuffle(); // It generates a random order for every item on the copyList
+    return copyList;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/answer_button.dart';
+import 'package:quizapp/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,33 +12,39 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+    var actualQuestionNumber = 0;
+
+    void changeQuestion(){
+      setState(() {
+        actualQuestionNumber++;
+      });
+    }
   @override
   Widget build(context) {
+
+    final objectQuestAns = questions[actualQuestionNumber];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Question 1 here"),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 1"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 2"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 3"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 4"),
-          ),
-    
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              objectQuestAns.question,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            // Extracting data from the array questions and then put into the AnswerButton class
+            ...objectQuestAns.getSuffledAnswers().map((items) {
+              return AnswerButton(items, changeQuestion);
+            }),
+          ],
+        ),
       ),
     );
   }
