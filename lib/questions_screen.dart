432b3e8d60a4ffe8e8b41bb157477848cfcdcd0c
Jairo <jn.gomezm1@unaindes.edu.co>
2023-07-26 14:32:09 -0500

    checkpoint question & answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
-
-class QuestionsScreen extends StatefulWidget{
-
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
-  State<QuestionsScreen> createState(){
+  State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
 }
 
-
-class _QuestionsScreenState extends State<QuestionsScreen>{
-
-
+class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
-  Widget build(context){
-    return const Text("Hi");
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Question 1 here"),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer 1"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer 2"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer 3"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer 4"),
+          ),
+    
+        ],
+      ),
+    );
   }
 }
