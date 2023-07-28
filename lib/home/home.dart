f1a75bea39025929bf825ce3a1fe96bef5e7043a
Jairo <jn.gomezm1@unaindes.edu.co>
2023-07-28 12:00:07 -0500

    Home done

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
-
-class Home extends StatelessWidget{
-
+class Home extends StatelessWidget {
   const Home({super.key});
-
   @override
-  Widget build(context){
-    return Container(
-      alignment: Alignment.center,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 99, 10, 168),
-          Color.fromARGB(255, 141, 19, 234)
-        ])
+  Widget build(context) {
+    return Center( // Puts whatever we put in, in the center of the page
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // The Column will take the minimun amount of space, instead of taking all the height space
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+              width: 200, color: const Color.fromARGB(128, 255, 255, 255)),
+          const SizedBox(height: 60,), // Add some spacing between widgets
+          const Text("Learn Flutter the fun way!",style: TextStyle(
+              color: Colors.white,
+              fontSize: 28
+            ),
+          ),
+          const SizedBox(height: 100,),
+          OutlinedButton.icon(
+              onPressed: (){},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon:const Icon(Icons.arrow_right_alt),
+              label: const Text("Start Quiz",style: TextStyle(fontSize:20)),
+          ),
+        ],
       ),
     );
   }
-
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/home/home.dart';
-void main(){
+
+void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Home()
-      )
-    )
+        body: Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 99, 10, 168),
+                Color.fromARGB(255, 141, 19, 234)
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+            ),
+            child: const Home() // Todas las paginas van a tener esta configuracion de color en el background
+        ),
+      ),
+    ),
   );
 }
